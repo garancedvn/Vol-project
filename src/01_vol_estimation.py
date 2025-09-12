05f9c28c0d9f892fd3cf7265d2f569d27acd8222
garancedvn <danvin.garance@gmail.com>
2025-09-12 17:54:43 +0100

    PnL calculations part 1

--- src/01_vol_estimation.py
+++
@@ -1,9 +0,0 @@
-countries = [['France', 65], ['Germany', 80], ['Italy', 60]]
-for item in countries:
-    item[1] += 1
-print(countries[1][1])
-
-populations = []
-for item in countries:
-     populations.append(item[1])
-print(populations)
--- src/02_signal_and_bt.py
+++ src/02_signal_and_bt.py
@@ -13,7 +13,7 @@ forecast_series = pd.Series(index=rets.index, dtype=float)
 
 for t in range(rolling_window, len(rets) - 21):
     r_train = rets.iloc[:t]*100
-    am = arch_model(r_train, vol="Garch", p=1, q=1, dist= "normal")
+    am = arch_model(r_train, vol="Garch", p=1, q=1, dist= "normal").fit()
     var1 = am.forecast(horizon=1).variance.values[-1,0] / (100**2)
     garch21 = np.sqrt(252 *21 * var1)
     forecast_series.iloc[t] = garch21
@@ -25,3 +25,41 @@ buffer = 0.02
 BASE["signal"] = 0 
 BASE.loc[BASE["GARCH_RV21_forecast"] > BASE["IV_30d_proxy"] + buffer, "signal"] = 1 
 BASE.loc[BASE["GARCH_RV21_forecast"] < BASE["IV_30d_proxy"] - buffer, "signal"] = -1 
+BASE["side"] = BASE["signal"]
+
+# PnL proxy
+scale = 1    
+cost = 1
+entries_enter_trade = BASE[(BASE["signal"] != 0) & (BASE["signal"].shift(1).fillna(0)==0)].copy()
+trades = []
+
+for dt in entries_enter_trade:
+    pos = BASE.index.get_loc(dt)
+    exit_pos = pos + 21
+
+    if exit_pos >= len(BASE.index):
+        continue 
+
+    exit_dt = BASE.index[exit_pos]
+
+    side = BASE.at[dt, "side "]
+    iv_entry = BASE.at[dt, "IV_30d_proxy"]
+    rv21_forecast = BASE.at[dt, "GARCH_RV21_forecast"]
+
+    if pd.isna(side) or pd.isna(iv_entry) or pd.isna(rv_fwd):
+        continue
+
+    pnl = ((rv21_forecast - iv_entry) * side * scale)
+
+    trades.append({
+        "entry_dt" : dt,
+        "exit_dt": exit_dt,
+        "side": side, 
+        "iv_entry": iv_entry, 
+        "rv_fwd": rv21_forecast,
+        "pnl": pnl
+
+    })
+    
+trades_df = pd.DataFrame(trades).set_index("entry_dt").sort_index()
+
